059ff3ac12502e5b5fef6c157bf358202f6f51a8
Anna Holov <holovanna25@gmail.co>
2023-10-08 20:49:27 +0300

    fixed version of lessonF

---
+++ src/main/java/org/example/Contact.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Contact {
+
+        String name;
+        String lastName;
+        String phoneNumber;
+
+        public Contact() {
+        }
+
+        public Contact(String name, String lastName, String phoneNumber) {
+            this.name = name;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+        }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,85 +1,130 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Contact> storage = new ArrayList<Contact>();
+
     public static void main(String[] args) throws IOException {
-        Contact contact1 = new Contact();
-        System.out.println("Input number:");
+        menu();
+
+    }
+
+    static void menu() throws IOException {
+        System.out.println("Input number of operation:");
+        System.out.println("1-Add");
+        System.out.println("2-List:");
+        System.out.println("3-Edit");
+        System.out.println("4-Delete");
+        System.out.println("5-Exit");
         int x = System.in.read();
 
         char ch = (char) x;
         switch (ch) {
             case '1':
-                contact1.addContact();
+                addContact();
                 break;
             case '2':
-                contact1.ListContacts();
+                listContacts();
                 break;
             case '3':
-                contact1.EditContact();
+                editContact();
                 break;
             case '4':
-                contact1.DeleteContact();
+                deleteContact();
             case '5':
-                contact1.ExitProgram();
+                exitProgram();
             default:
                 System.out.println("Wrong number");
                 break;
         }
     }
 
-    static class Contact {
-        String Name;
-        String LastName;
-        int PhoneNumber;
+    public static void addContact() throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        public Contact() {
-        }
+        System.out.println("Enter a name: ");
+        String name = sc.next();
 
-        public Contact(String name, String lastName, int phoneNumber) {
-            Name = name;
-            LastName = lastName;
-            PhoneNumber = phoneNumber;
-        }
+        System.out.println("Enter a lastname: ");
+        String lastName = sc.next();
 
-        public void addContact() {
-            System.out.println("New contact added.");
-            System.out.println("Name: " + Name + "Lastname: " + LastName + "Number: " + PhoneNumber);
+        System.out.println("Enter a phone number: ");
+        String phoneNumber = sc.next();
 
+        Contact contact = new Contact(name, lastName, phoneNumber);
+        System.out.println("Contact " + contact.name + " " + contact.lastName + " " + contact.phoneNumber + " added");
+        storage.add(contact);
+        menu();
+    }
 
-        }
+    public static void listContacts() throws IOException {
+        System.out.println("List of all contacts.");
+        listAllContactFromStorage();
+        menu();
+    }
 
-        public void ListContacts() {
-            System.out.println("List of all contacts.");
+    private static void listAllContactFromStorage() {
+        int i = 0;
 
-
-        }
-
-        public void EditContact() {
-            System.out.println("Contact edited.");
-            System.out.println("Name: " + Name + "Lastname: " + LastName + "Number: " + PhoneNumber);
-
-
-        }
-
-        public void DeleteContact() {
-            System.out.println("Contact deleted.");
-
-        }
-
-        public void ExitProgram() throws IOException {
-            System.out.println("If you want to exit type 0.");
-            System.out.println("Input number:");
-            int y = System.in.read();
-            if (y == 48) {
-                System.exit(0);
-            }
-
+        for (Contact contact : storage) {
+            System.out.println("Contact: " + i + " - " + contact.name + " " + contact.lastName + " " + contact.phoneNumber);
+            i++;
         }
     }
 
+    public static void editContact() throws IOException {
+
+        listAllContactFromStorage();
+        System.out.println("Input number to edit contact");
+
+        Scanner in = new Scanner(System.in);
+        int index = in.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a name: ");
+        String name = sc.next();
+
+        System.out.println("Enter a lastname: ");
+        String lastName = sc.next();
+
+        System.out.println("Enter a phone number: ");
+        String phoneNumber = sc.next();
+        storage.set(index,new Contact(name,lastName,phoneNumber));
+
+        System.out.println("Contact edited.");
+        menu();
+
+    }
+
+    public static void deleteContact() throws IOException {
+        listAllContactFromStorage();
+        System.out.println("Input number to delete contact");
+
+        Scanner in = new Scanner(System.in);
+        int index = in.nextInt();
+
+        storage.remove(index);
+        System.out.println("Contact deleted.");
+        menu();
+    }
+
+    public static void exitProgram() throws IOException {
+        System.out.println("If you want to exit type 0.");
+        System.out.println("Input number:");
+        int y = System.in.read();
+        if (y == 48) {
+            System.exit(0);
+        }
+
+
+    }
 }
+
+
 
 
 
